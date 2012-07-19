c4b03e87a4251d8326822b52b73e773b67ae073a
Eduardo Bohrer <nbluisrs@gmail.com>
2012-07-19 18:55:33 -0300

    adicionado teste que seta valor no campo texto

--- spec/googledocs_textfield_spec.rb
+++ spec/googledocs_textfield_spec.rb
@@ -4,19 +4,24 @@ require 'rspec'
 
 describe "Look for browser elements" do
 
-
-	before(:each) {puts 'iniciou'}
-
-	after(:each) {}
-
-
-	it "Fill first name field" do
-        # Cria uma instancia do firefox e abre o formulário no google docs
-		firefox = Watir::Browser.new :firefox
-		firefox.goto 'https://docs.google.com/spreadsheet/viewform?formkey=dHFQdmltaDdaYkFjVVVwd1dQXzJQSEE6MQ#gid=0'
-		first_field = firefox.text_field :id => 'entry_0'
-		first_field.exists?.should be(true)
-		firefox.close
-
+	before(:each) do
+	  @firefox = Watir::Browser.new :firefox
+		@firefox.goto 'https://docs.google.com/spreadsheet/viewform?formkey=dHFQdmltaDdaYkFjVVVwd1dQXzJQSEE6MQ#gid=0'
 	end
+
+	after(:each) do
+	  @firefox.close
+	end
+
+	it "Check if the first field exists" do
+		first_field = @firefox.text_field :id => 'entry_0'
+		first_field.exists?.should be(true)
+	end
+	
+	it "Fill the first field" do
+	  first_field = @firefox.text_field :id => 'entry_0'
+	  first_field.when_present.set 'Luiza foi para o Canada, denovo.'
+	  first_field.value.should == 'Luiza foi para o Canada, denovo.'
+	end
+
 end
